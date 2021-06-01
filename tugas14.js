394b38885f79563b12a08b9e14d8c9950f2f624d
EffenrilAgung <66568136+EffenrilAgung@users.noreply.github.com>
2021-06-01 20:46:52 +0700

    Initial commit

---
+++ tugas14.js
@@ -0,0 +1,9 @@
+var tanggallahir = 24
+var hasil = 37 / tanggallahir
+console.log(hasil)
+console.log("")
+console.log(Math.round(hasil))
+console.log("")
+console.log(Math.ceil(hasil))
+console.log("")
+console.log(Math.floor(hasil))
